344fffaa6ca15990289061a1a657eacc51e086e3
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2023-03-25 20:25:04 +0630

    Initial commit

---
+++ JavaSE4thBatch/src/main/java/com/turing/javase4thbatch/JavaSE4thBatch.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.turing.javase4thbatch;
+
+/**
+ *
+ * @author macboo
+ */
+public class JavaSE4thBatch {
+
+    public static void main(String[] args) {
+        
+        int a = 15, b= 18;
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
---
+++ dynamic.js
@@ -0,0 +1,12 @@
+var a = 3/2;
+console.log('A ',a);
+
+a = "Hello" * 4;
+console.log('A ',a);
+console.log('Type of ',typeof a);
+
+a = false;
+console.log('Type of ',typeof a);
+
+a = "Hello";
+console.log('Type of ',typeof a);
